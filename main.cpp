34056253cdc00738613505fd19f6777908e45c8c
Ilja Gabbasovs <ilja.gabbasovs@doglabs.co.uk>
2021-04-13 12:50:16 +0100

    Adds main.cpp

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <SDL.h>
+
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+int main(int argc, char* args[]) {
+    SDL_Window* window = nullptr;
+    SDL_Surface* screenSurface = nullptr;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "SDL couldn't init video: " << SDL_GetError() << std::endl;
+    } else {
+        window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == nullptr) {
+            std::cout << "Window couldn't be created: " << SDL_GetError() << std::endl;
+        } else {
+            screenSurface = SDL_GetWindowSurface(window);
+            SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0x00, 0xFF, 0xFF));
+            SDL_UpdateWindowSurface(window);
+        }
+    }
+
+    SDL_Event e;
+    bool quit = false;
+    while (!quit){
+        while (SDL_PollEvent(&e)){
+            if (e.type == SDL_QUIT){
+                quit = true;
+            }
+            if (e.type == SDL_KEYDOWN){
+                quit = true;
+            }
+            if (e.type == SDL_MOUSEBUTTONDOWN){
+                quit = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
+    return 0;
+}
